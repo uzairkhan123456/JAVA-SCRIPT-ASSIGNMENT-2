7ea9aea3532e4b721a7ad467f3f69f44686b070a
uzair-khan <uzair722200@gmail.com>
2023-03-30 00:58:25 -0700

    FOR LOOP SEARCH

--- app.js
+++ app.js
@@ -26,4 +26,23 @@
 // 			.innerHTML = currentTime;
 // }
 // showTime();
-document.getElementById
+// document.getElementById
+
+// var table = prompt("write a num")
+// for( var i = 1; i <= 10 ; i++ ){
+//     console.log( table + " x " + i + " = " + i*table )
+// }
+var cities = [ "lahore" , "karachi", "islamabad", "peshawer","qutta"];
+var city = prompt("write a city name" )
+var found = false 
+for ( var  i = 0; i < cities.length; i++ ){
+    if( city.toLowerCase() === cities[i].toLowerCase()   ){
+        console.log(" Found " + city);
+        found = true 
+        break;
+
+    }
+}
+if(!found){
+    console.log( " not found " + city );
+}
