c61a722a2eecf70190ffae6efaec7cff6799c5e4
uzair-khan <uzair722200@gmail.com>
2023-04-07 06:40:22 -0700

    java script assignment

--- app.js
+++ app.js
@@ -1,48 +1,111 @@
-// setInterval(showTime, 1000);
-// function showTime() {
-// 	let time = new Date();
-// 	let hour = time.getHours();
-// 	let min = time.getMinutes();
-// 	let sec = time.getSeconds();
-// 	am_pm = "AM";
+// //QUESTION NO 1
 
-// 	if (hour > 12) {
-// 		hour -= 12;
-// 		am_pm = "PM";
-// 	}
-// 	if (hour == 0) {
-// 		hr = 12;
-// 		am_pm = "AM";
-// 	}
+// let number1 = prompt("Enter the first number:");
+// let number2 = prompt("Enter the second number:");
 
-// 	hour = hour < 10 ? "0" + hour : hour;
-// 	min = min < 10 ? "0" + min : min;
-// 	sec = sec < 10 ? "0" + sec : sec;
-
-// 	let currentTime = hour + ":"
-// 			+ min + ":" + sec + am_pm;
-
-// 	document.getElementById("clock")
-// 			.innerHTML = currentTime;
+// if (number1 > number2) {
+//   console.log(number1 );
+// } else if (number2 > number1) {
+//   console.log(number2   );
+// } else {
+//   console.log("Both numbers are equal.");
 // }
-// showTime();
-// document.getElementById
 
-// var table = prompt("write a num")
-// for( var i = 1; i <= 10 ; i++ ){
-//     console.log( table + " x " + i + " = " + i*table )
+// //QUESTION NO 2
+
+// let number = prompt("Enter a number:"); 
+// let sign;
+
+// if (number > 0) {
+//   sign = "+";
+// } else if (number < 0) {
+//   sign = "-";
+// } else {
+//   sign = "zero";
 // }
-var cities = [ "lahore" , "karachi", "islamabad", "peshawer","qutta"];
-var city = prompt("write a city name" )
-var found = false 
-for ( var  i = 0; i < cities.length; i++ ){
-    if( city.toLowerCase() === cities[i].toLowerCase()   ){
-        console.log(" Found " + city);
-        found = true 
-        break;
 
-    }
-}
-if(!found){
-    console.log( " not found " + city );
-}
+// console.log("The sign  is " + sign);
+
+// //QUESTION NO 3
+
+// let num1 = prompt("entetr the 1st number");
+// let num2 = prompt("entetr the 2st number");
+// let num3 = prompt("entetr the 3st number");
+// let num4 = prompt("entetr the 4st number");
+// let num5 = prompt("entetr the 5st number");
+
+// if( num1 > num2 ){
+//     console.log(num1);
+// } else if (num2 > num3){
+//    console.log(num2);
+// }else if (num3 > num4 ){
+//    console.log(num3);
+// } else if ( num4 > num5){
+//    console.log( num4);
+//  } else if ( num5 > num1){
+//     console.log(num5);
+//  } else {
+//     console.log("both num are not true ")
+//  }
+
+//  //QUESTION NO 4
+
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i + " is even");
+//     } else {
+//       console.log(i + " is odd");
+//     }
+//   }
+
+//  //QUESTION NO 5
+
+// var num = +prompt("Type your percentage");
+// var grade;
+
+// if (num >= 0 && num <= 100) {
+//   if (num >= 80) {
+//     grade = "A+";
+//   } else if (num >= 70) {
+//     grade = "A";
+//   } else if (num >= 60) {
+//     grade = "B";
+//   } else if (num >= 50) {
+//     grade = "C";
+//   } else {
+//     grade = "D";
+//   }
+//   console.log(grade);
+// } else {
+//     console.log("Wrong input");
+// }
+
+//  //QUESTION NO 6
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+
+
+//  //QUESTION NO 7
+
+// const rows = 5;
+// for (let i = 1; i <= rows; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += "* ";
+//   }
+//   console.log(row);
+// }
+
+
+  
+  
